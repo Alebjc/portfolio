205c3a305b89909f0b9a9fe2f479b0bc0202b084
Alexis <juniorbasarte@gmail.com>
2023-10-13 20:19:56 -0400

    gitflow-feature-stash: update-main

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -12,28 +12,38 @@ const Home = () => {
   const toggleShow = () => setShow(!show);
     
     return (
-        <div className="container home-page">
-            <div className="text-zone">
-                <h1>
-                    Hi, I'm <AnimatedLetters className='animate-color' letterClass={letterClass}
-                    strArray={nameArray}
-                    idx={15}/>
-                </h1>
-                <h2>
-                    Computer Systems Student
-                </h2>
-                <Link to="/contact" className="flat-button">Contact me</Link>
-            </div>
-            <TERipple rippleColor="light">
-                <a
-                  className="inline-block rounded bg-primary mr-2 px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
-                  role="button"
-                  onClick={toggleShow}
-                >
-                    Link
-                </a>
-            </TERipple>
+      <div className="container home-page">
+        <div className="text-zone">
+          <h1>
+            Hi, I'm{' '}
+            <AnimatedLetters
+              className="animate-color"
+              letterClass={letterClass}
+              strArray={nameArray}
+              idx={15}
+            />
+          </h1>
+          <h2>Computer Systems Student</h2>
+          <Link to="/contact" className="flat-button">
+            Contact me
+          </Link>
         </div>
+        <TERipple rippleColor="light">
+          <a
+            className="inline-block rounded bg-primary mr-2 px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
+            role="button"
+            onClick={toggleShow}
+          >
+            Link
+          </a>
+        </TERipple>
+        <div
+          class="mb-4 rounded-lg bg-primary-100 px-6 py-5 text-base text-primary-600"
+          role="alert"
+        >
+          A simple primary alert—check it out!
+        </div>
+      </div>
     )
 }
 
